4def7edc3489648a00e9c1cbc053f021185de8b6
Adam Goldsmith <adam@adamgoldsmith.name>
2015-05-14 12:26:51 -0400

    switch to list of pins that are being used, wait for input to finish before moving to next button

--- config.py
+++ config.py
@@ -4,33 +4,30 @@ import RPi.GPIO as GPIO
 
 GPIO.setmode(GPIO.BOARD)
 
-pinMin=1
-pinMax=26
+pins=[7,8,11,12,13,15,16,18,19,21,22,23,24,26,29,31,32,33,37,38]
 
 buttons=["Up","Down","Left","Right","1","2","3","4","5","6"]
 
 file=open("outfile.txt", "w")
 
-for i in range(pinMin,pinMax):
-    GPIO.setup(i, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+for i in pins:
+    try:
+        GPIO.setup(i, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+    except:
+        print "ouch", i
 
-print("Player 1:");
-for button in buttons:
-    print(button)
-    for i in range(pinMin,pinMax):
-        if (not GPIO.input(i)):
-            print(i)
-            file.write(i)
-            break
-        time.sleep(.02);
 
-file.write('\n')
-print("Player 2:");
 for button in buttons:
-    print(button)
-    for i in range(pinMin,pinMax):
-        if (not GPIO.input(i)):
-            print(i)
-            file.write(i)
-            break
-        time.sleep(.02);
+    print button
+    done = False
+    while not done:
+        for i in pins:
+            if (not GPIO.input(i)):
+                print(i)
+                file.write(str(i) + "\n")
+                while not GPIO.input(i):
+                    print "asdifhluiasdhlfkhasldkjfha"
+                    time.sleep(.02)
+                done = True
+                break
+            time.sleep(.02)
