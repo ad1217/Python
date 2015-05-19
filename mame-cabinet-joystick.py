eabd5559740abeac96bb895f228afed9b0663d81
Adam Goldsmith <adam@adamgoldsmith.name>
2015-05-19 17:39:58 -0400

    experimental code for charlieplexing

--- mame-cabinet-joystick.py
+++ mame-cabinet-joystick.py
@@ -12,10 +12,18 @@ GPIO.setmode(GPIO.BOARD)
 #Second dictionary to store state
 isPressed = {}
 
+CharliePins = ([])
+
 # initialize keys
 for pin in keymap.keys():
   GPIO.setup(pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
   isPressed[pin] = False
+
+for pins in CharlieMap:
+  CharliePins =CharliePins.update(pins)
+  for pin in pins:
+    GPIO.setup(pin, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+    isPressed[pin] = False
 
 def checkPressed (state, key, value):
   value[0].emit(value[1], value[2])
@@ -29,4 +37,11 @@ def checkPressed (state, key, value):
 while True:
   for key, value in keymap.items():
     checkPressed((not GPIO.input(key)), key, value)
+
+  for key, value in CharlieMap.items():
+    for pin in CharliePins:
+      GPIO.setup(pin, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+    GPIO.setup(key[0], GPIO.OUT)
+    GPIO.output(key[0], GPIO.HIGH
+    checkPressed(GPIO.input(key[1]), key, value):
   time.sleep(.01)  # Poll every 10ms (otherwise CPU load gets too high)
